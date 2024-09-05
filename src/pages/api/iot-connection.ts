db79302ed08f368b4f66ab5136288a0f02bb1262
timf34 <farrelti@tcd.ie>
2024-09-05 16:40:52 +0100

    test-iot-connection.ts is working

--- src/pages/api/iot-connection.ts
+++ src/pages/api/iot-connection.ts
@@ -18,7 +18,7 @@ const connectToIoT = () => {
     return new Promise((resolve, reject) => {
         iotDevice.on('connect', () => {
             console.log('Connected to AWS IoT');
-            iotDevice.subscribe('fov/devices/+/status');
+            iotDevice.subscribe('ap-southeast-2/marvel/fov-marvel-tablet-1/+');
             resolve(iotDevice);
         });
 
@@ -40,7 +40,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 await connectToIoT();
                 iotDevice.on('message', (topic: string, payload: Buffer) => {
                     const data = JSON.parse(payload.toString());
-                    io.emit('iot-data', data);
+                    console.log('Received message:', topic, data);
+                    const topicParts = topic.split('/');
+                    const dataType = topicParts[topicParts.length - 1];
+                    io.emit('iot-data', { type: dataType, value: data });
                 });
             } catch (error) {
                 console.error('Failed to connect to AWS IoT:', error);
@@ -52,3 +55,38 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
+
+const SocketHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (res.socket.server.io) {
+        console.log('Socket is already running');
+        res.end();
+        return;
+    }
+
+    console.log('Setting up socket');
+    const io = new SocketServer(res.socket.server);
+    res.socket.server.io = io;
+
+    io.on('connection', (socket) => {
+        console.log('New client connected');
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnected');
+        });
+    });
+
+    try {
+        await connectToIoT();
+        iotDevice.on('message', (topic: string, payload: Buffer) => {
+            const data = JSON.parse(payload.toString());
+            console.log('Received message:', topic, data);
+            const topicParts = topic.split('/');
+            const dataType = topicParts[topicParts.length - 1];
+            io.emit('iot-data', { type: dataType, value: data });
+        });
+    } catch (error) {
+        console.error('Failed to connect to AWS IoT:', error);
+    }
+
+    res.end();
+};
